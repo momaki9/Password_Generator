8598febb202edc4f2c6140063bf721cc13a3e1bd
Mostafa Maki <mostafa_m9@yahoo.com>
2022-07-23 18:23:35 -0700

    added comments in the script file describing some of the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
-// Assignment Code
+// variable declarations needed for password generating and writing
+// the variables generateBtn and passwordContent are set to the contents the of the elements with IDs of generate and password, respectively
 var generateBtn = document.querySelector("#generate");
 var passwordContent = document.querySelector("#password");
+// library of characters used to generate the password
 var charSym = "!@#$%^&*()";
 var charNum = "01234567890";
 var charCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var charLow = "abcdefghijklmnopqrstuvwxyz";
+// empty variables declared such that it could be 'filled' based on user input 
 var chars = "";
 var passwordText = "";
 
-// Write password to the #password input
+// this is the function that writes the generated password to the text area (has ID of #password)
 function writePassword() {
   var password = generatePassword();
   passwordContent.value = password;
   document.getElementById('password').value= passwordText;
 }
 
-// generates password
+// this is the function that generates the password
 function generatePassword() {
   passwordText = "";
   chars = "";
   var passwordLength = window.prompt("Enter the number of characters you would like for the Password. \n\n(Minimum of 8 are required. Max is 128)");
   if (passwordLength === null) {
+    // this allows the user to quit the password generation process
     var passGenExit = window.confirm("Quit?");
     if (passGenExit) {
       return;
@@ -29,16 +33,17 @@ function generatePassword() {
       return generatePassword();
     }
   }
+  // if a number outside the required range is inputted, the user recieves an alert
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Not a vaild entry! Please enter a number between 8 and 128.");
     return generatePassword();
   }
-  
+  // if text is inputted instead of a number, the user recieves an alert
   if (isNaN(passwordLength)) {
     window.alert("Not a vaild entry!");
     return generatePassword();
   }
-   
+  /* these variables correlate to each possible character type, if selected, the list of available characters increase using the concat method; otherwise if the user selects no to one or more character type then that character type is excluded */
   charSymInclude = window.confirm("OK to include symbols(i.e. % & ! $))? ")
   if (charSymInclude) {
     chars = chars.concat(charSym);
@@ -58,12 +63,13 @@ function generatePassword() {
   if (charLowInclude) {
     chars = chars.concat(charLow);
   }
-
+  // if no character type is selected, then the user is alerted and asked to select at least one type
   if (!charCapInclude && !charLowInclude && !charNumInclude && !charSymInclude) {
     window.alert("Please try again and be sure to select at least one character type.")
     return generatePassword();
   }
   
+  // randomization needed to create a more secure password
   for (var i = 0; i < passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
     passwordText += chars.substring(randomNumber, randomNumber +1);
@@ -71,5 +77,5 @@ function generatePassword() {
   
 }
 
-// Add event listener to generate button
+// Add event listener to generate button such that when a user clicks the button the function starts
 generateBtn.addEventListener("click", writePassword);
